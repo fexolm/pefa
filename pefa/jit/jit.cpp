aea0ced1faaa952943173a9d3198a9395d08dcb9
Artem Alekseev <fexolm@gmail.com>
2020-06-09 15:55:13 +0300

    Simplify jit. Remove compile on demand layer

--- pefa/jit/jit.cpp
+++ pefa/jit/jit.cpp
@@ -1,37 +1,35 @@
-//
-// Created by fexolm on 06.06.2020.
-//
-
 #include "jit.h"
-#include "llvm/Transforms/Vectorize.h"
-#include "llvm/Transforms/IPO/AlwaysInliner.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Transforms/IPO.h"
-#include "llvm/PassRegistry.h"
-#include "llvm/InitializePasses.h"
+
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/OrcV1Deprecation.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Mangler.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/InitializePasses.h>
+#include <llvm/PassRegistry.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/IPO.h>
+#include <llvm/Transforms/IPO/AlwaysInliner.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/GVN.h>
+#include <llvm/Transforms/Vectorize.h>
 
 namespace pefa::jit {
 JIT::JIT()
-    : m_target_machine(EngineBuilder().selectTarget()),
-      m_data_layout(m_target_machine->createDataLayout()),
+    : m_target_machine(EngineBuilder().selectTarget()), m_data_layout(m_target_machine->createDataLayout()),
       m_object_layer(AcknowledgeORCv1Deprecation, m_session,
                      [this](VModuleKey K) {
-                       return LegacyRTDyldObjectLinkingLayer::Resources{
-                           std::make_shared<SectionMemoryManager>(), m_resolvers[K]};
+                       return LegacyRTDyldObjectLinkingLayer::Resources{std::make_shared<SectionMemoryManager>(), m_resolvers[K]};
                      }),
-      m_compile_layer(AcknowledgeORCv1Deprecation, m_object_layer,
-                      SimpleCompiler(*m_target_machine)),
+      m_compile_layer(AcknowledgeORCv1Deprecation, m_object_layer, SimpleCompiler(*m_target_machine)),
       m_optimize_layer(AcknowledgeORCv1Deprecation, m_compile_layer,
-                       [this](std::unique_ptr<Module> M) { return optimizeModule(std::move(M)); }),
-      m_compile_callback_manager(cantFail(
-          createLocalCompileCallbackManager(m_target_machine->getTargetTriple(), m_session, 0))),
-      m_cod_layer(
-          AcknowledgeORCv1Deprecation, m_session, m_optimize_layer,
-          [&](VModuleKey K) { return m_resolvers[K]; },
-          [&](VModuleKey K, std::shared_ptr<SymbolResolver> R) { m_resolvers[K] = std::move(R); },
-          [](Function &F) { return std::set<Function *>({&F}); }, *m_compile_callback_manager,
-          createLocalIndirectStubsManagerBuilder(m_target_machine->getTargetTriple())) {
+                       [this](std::unique_ptr<Module> M) { return optimizeModule(std::move(M)); }) {
   llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
 }
 TargetMachine &JIT::getTargetMachine() {
@@ -39,7 +37,6 @@ TargetMachine &JIT::getTargetMachine() {
 }
 VModuleKey JIT::addModule(std::unique_ptr<Module> M) {
   VModuleKey K = m_session.allocateVModule();
-
   m_resolvers[K] = createLegacyLookupResolver(
       m_session,
       [this](StringRef Name) -> JITSymbol {
@@ -53,20 +50,20 @@ VModuleKey JIT::addModule(std::unique_ptr<Module> M) {
       },
       [](Error Err) { cantFail(std::move(Err), "lookupFlags failed"); });
 
-  cantFail(m_cod_layer.addModule(K, std::move(M)));
+  cantFail(m_optimize_layer.addModule(K, std::move(M)));
   return K;
 }
 JITSymbol JIT::findSymbol(std::string Name) {
   std::string MangledName;
   raw_string_ostream MangledNameStream(MangledName);
   Mangler::getNameWithPrefix(MangledNameStream, Name, m_data_layout);
-  return m_cod_layer.findSymbol(MangledNameStream.str(), true);
+  return m_optimize_layer.findSymbol(MangledNameStream.str(), true);
 }
 void JIT::removeModule(VModuleKey K) {
-  cantFail(m_cod_layer.removeModule(K));
+  cantFail(m_optimize_layer.removeModule(K));
 }
 std::unique_ptr<Module> JIT::optimizeModule(std::unique_ptr<Module> M) {
-  auto FPM = std::make_unique<FunctionPassManager>(M.get());
+  auto FPM = std::make_unique<llvm::legacy::FunctionPassManager>(M.get());
   auto MPM = std::make_unique<llvm::legacy::PassManager>();
   using namespace llvm;
   PassManagerBuilder Builder;
@@ -80,13 +77,12 @@ std::unique_ptr<Module> JIT::optimizeModule(std::unique_ptr<Module> M) {
   m_target_machine->adjustPassManager(Builder);
   Builder.populateFunctionPassManager(*FPM);
   M->print(llvm::errs(), nullptr);
-  MPM->run(*M);
-  for (auto &F : *M)
-    FPM->run(F);
-  MPM->run(*M);
-  for (auto &F : *M)
-    FPM->run(F);
 
+  FPM->doInitialization();
+  for (auto &F : *M)
+    FPM->run(F);
+  FPM->doFinalization();
+  MPM->run(*M);
   M->print(llvm::errs(), nullptr);
   return M;
 }
@@ -98,44 +94,6 @@ std::shared_ptr<JIT> get_JIT() {
     llvm::InitializeNativeTarget();
     llvm::InitializeNativeTargetAsmPrinter();
     llvm::InitializeNativeTargetAsmParser();
-    using namespace llvm;
-    // Initialize passes
-    PassRegistry &Registry = *PassRegistry::getPassRegistry();
-    initializeCore(Registry);
-    initializeCoroutines(Registry);
-    initializeScalarOpts(Registry);
-    initializeObjCARCOpts(Registry);
-    initializeVectorization(Registry);
-    initializeIPO(Registry);
-    initializeAnalysis(Registry);
-    initializeTransformUtils(Registry);
-    initializeInstCombine(Registry);
-    initializeAggressiveInstCombine(Registry);
-    initializeInstrumentation(Registry);
-    initializeTarget(Registry);
-    // For codegen passes, only passes that do IR to IR transformation are
-    // supported.
-    initializeExpandMemCmpPassPass(Registry);
-    initializeScalarizeMaskedMemIntrinPass(Registry);
-    initializeCodeGenPreparePass(Registry);
-    initializeAtomicExpandPass(Registry);
-    initializeRewriteSymbolsLegacyPassPass(Registry);
-    initializeWinEHPreparePass(Registry);
-    initializeDwarfEHPreparePass(Registry);
-    initializeSafeStackLegacyPassPass(Registry);
-    initializeSjLjEHPreparePass(Registry);
-    initializePreISelIntrinsicLoweringLegacyPassPass(Registry);
-    initializeGlobalMergePass(Registry);
-    initializeIndirectBrExpandPassPass(Registry);
-    initializeInterleavedLoadCombinePass(Registry);
-    initializeInterleavedAccessPass(Registry);
-    initializeEntryExitInstrumenterPass(Registry);
-    initializePostInlineEntryExitInstrumenterPass(Registry);
-    initializeUnreachableBlockElimLegacyPassPass(Registry);
-    initializeExpandReductionsPass(Registry);
-    initializeWasmEHPreparePass(Registry);
-    initializeWriteBitcodePassPass(Registry);
-    initializeHardwareLoopsPass(Registry);
     jit = std::make_shared<JIT>();
     return jit;
   }
--- pefa/jit/jit.h
+++ pefa/jit/jit.h
@@ -1,26 +1,14 @@
 #pragma once
-#include <llvm/ADT/STLExtras.h>
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/ExecutionEngine/JITSymbol.h>
-#include <llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h>
 #include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include <llvm/ExecutionEngine/Orc/Core.h>
 #include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
 #include <llvm/ExecutionEngine/Orc/IRTransformLayer.h>
-#include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
+#include <llvm/ExecutionEngine/Orc/Legacy.h>
 #include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
-#include <llvm/ExecutionEngine/RTDyldMemoryManager.h>
-#include <llvm/ExecutionEngine/RuntimeDyld.h>
-#include <llvm/ExecutionEngine/SectionMemoryManager.h>
 #include <llvm/IR/DataLayout.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Mangler.h>
-#include <llvm/Support/DynamicLibrary.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Module.h>
 #include <llvm/Target/TargetMachine.h>
-#include <llvm/Transforms/InstCombine/InstCombine.h>
-#include <llvm/Transforms/Scalar.h>
-#include <llvm/Transforms/Scalar/GVN.h>
 
 // TODO: Current jit implementation is copy paste of llvm::KaleidoscopeJIT
 // it is legacy and should be refactored in future
@@ -28,37 +16,8 @@
 // TODO: Move includes to header
 
 namespace {
-using llvm::AcknowledgeORCv1Deprecation;
-using llvm::cantFail;
-using llvm::createCFGSimplificationPass;
-using llvm::createGVNPass;
-using llvm::createInstructionCombiningPass;
-using llvm::createReassociatePass;
-using llvm::DataLayout;
-using llvm::EngineBuilder;
-using llvm::Error;
-using llvm::Function;
-using llvm::JITSymbol;
-using llvm::JITSymbolFlags;
-using llvm::Mangler;
-using llvm::Module;
-using llvm::raw_string_ostream;
-using llvm::RTDyldMemoryManager;
-using llvm::SectionMemoryManager;
-using llvm::StringRef;
-using llvm::TargetMachine;
-using llvm::legacy::FunctionPassManager;
-using llvm::orc::createLocalCompileCallbackManager;
-using llvm::orc::createLocalIndirectStubsManagerBuilder;
-using llvm::orc::ExecutionSession;
-using llvm::orc::JITCompileCallbackManager;
-using llvm::orc::LegacyCompileOnDemandLayer;
-using llvm::orc::LegacyIRCompileLayer;
-using llvm::orc::LegacyIRTransformLayer;
-using llvm::orc::LegacyRTDyldObjectLinkingLayer;
-using llvm::orc::SimpleCompiler;
-using llvm::orc::SymbolResolver;
-using llvm::orc::VModuleKey;
+using namespace llvm;
+using namespace llvm::orc;
 } // namespace
 
 namespace pefa::jit {
@@ -74,9 +33,6 @@ private:
   using OptimizeFunction = std::function<std::unique_ptr<Module>(std::unique_ptr<Module>)>;
 
   LegacyIRTransformLayer<decltype(m_compile_layer), OptimizeFunction> m_optimize_layer;
-
-  std::unique_ptr<JITCompileCallbackManager> m_compile_callback_manager;
-  LegacyCompileOnDemandLayer<decltype(m_optimize_layer)> m_cod_layer;
 
 public:
   JIT();
