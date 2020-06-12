16ff9b291832c4c2fc7d8a86237722e8bcee5e79
Artem Alekseev <fexolm@gmail.com>
2020-06-12 16:54:47 +0300

    Better comments for filter kernel codegen

--- pefa/kernels/filter.cpp
+++ pefa/kernels/filter.cpp
@@ -89,14 +89,16 @@ private:
   void (*m_func)(const uint8_t *, uint8_t *, int64_t);
 
 public:
-  FitlerKernelImpl(const std::shared_ptr<const arrow::Field> field, const std::shared_ptr<const Expr> expr)
+  FitlerKernelImpl(const std::shared_ptr<const arrow::Field> field,
+                   const std::shared_ptr<const Expr> expr)
       : m_field(field)
       , m_expr(expr)
       , m_context(llvm::LLVMContext())
       , m_jit(jit::get_JIT())
       , utils::LLVMTypesHelper(m_context) {}
 
-  void execute(const std::shared_ptr<arrow::Array> column, uint8_t *bitmap, size_t offset) override {
+  void execute(const std::shared_ptr<arrow::Array> column, uint8_t *bitmap,
+               size_t offset) override {
     if (!m_is_compiled) {
       throw KernelNotCompiledException();
     }
@@ -137,8 +139,7 @@ private:
     llvm::Function *func = llvm::Function::Create(prototype, llvm::Function::InternalLinkage,
                                                   m_field->name() + "_predicate", module);
     llvm::BasicBlock *body = llvm::BasicBlock::Create(m_context, "body", func);
-    auto args = func->arg_begin();
-    llvm::Value *val = &(*args);
+    llvm::Value *val = func->getArg(0);
     llvm::IRBuilder builder(m_context);
     builder.SetInsertPoint(body);
 
@@ -148,14 +149,15 @@ private:
   }
 
   void gen_filter_func(llvm::Module &module) {
-    // void filter(int *in, char *out, int len) {
+    // void filter(uint8_t *in, uint8_t *out, uint64_t len) {
+    //     TYPE *source = (TYPE *)in;
     //     for(int i=0; i<len / 8; i++) {
-    //         int *src = in + i * 8;
-    //         char bitmap = 0;
+    //         int *src = source + i * 8;
+    //         uint8_t tmp_bitmap = 0;
     //         for(int j=0; j<8; j++) {
-    //            bitmap |= predicate(src[j]) << (7 - j);
+    //            tmp_bitmap |= predicate(src[j]) << (7 - j);
     //         }
-    //         out[i] &= bitmap;
+    //         out[i] &= tmp_bitmap;
     //     }
     // }
     std::vector<llvm::Type *> param_type{llvm::Type::getInt8PtrTy(m_context),
@@ -167,19 +169,16 @@ private:
     llvm::Function *func = llvm::Function::Create(prototype, llvm::Function::ExternalLinkage,
                                                   m_field->name() + "_filter", module);
     llvm::BasicBlock *body = llvm::BasicBlock::Create(m_context, "body", func);
-    llvm::BasicBlock *cond_1 = llvm::BasicBlock::Create(m_context, "cond_1", func);
-    llvm::BasicBlock *for_1 = llvm::BasicBlock::Create(m_context, "for_1", func);
-    llvm::BasicBlock *cond_2 = llvm::BasicBlock::Create(m_context, "cond_2", func);
-    llvm::BasicBlock *for_2 = llvm::BasicBlock::Create(m_context, "for_2", func);
-    llvm::BasicBlock *end_for1 = llvm::BasicBlock::Create(m_context, "end_for1", func);
-    llvm::BasicBlock *end_for2 = llvm::BasicBlock::Create(m_context, "end_for2", func);
+    llvm::BasicBlock *cond_1 = llvm::BasicBlock::Create(m_context, "outerloop.cond", func);
+    llvm::BasicBlock *for_1 = llvm::BasicBlock::Create(m_context, "outerloop.body", func);
+    llvm::BasicBlock *cond_2 = llvm::BasicBlock::Create(m_context, "innerloop.cond", func);
+    llvm::BasicBlock *for_2 = llvm::BasicBlock::Create(m_context, "innedloop.body", func);
+    llvm::BasicBlock *end_for1 = llvm::BasicBlock::Create(m_context, "outerloop.end", func);
+    llvm::BasicBlock *end_for2 = llvm::BasicBlock::Create(m_context, "innerloop.end", func);
 
-    auto args = func->arg_begin();
-    llvm::Value *arg_source = &(*args);
-    args = std::next(args);
-    llvm::Value *arg_dest = &(*args);
-    args = std::next(args);
-    llvm::Value *arg_len = &(*args);
+    llvm::Value *arg_source = func->getArg(0);
+    llvm::Value *arg_dest = func->getArg(1);
+    llvm::Value *arg_len = func->getArg(2);
 
     llvm::IRBuilder builder(m_context);
     builder.SetInsertPoint(body);
@@ -187,50 +186,64 @@ private:
     auto *j = builder.CreateAlloca(i8_typ(), nullptr, "j");
     auto *tmp_bitmap = builder.CreateAlloca(i8_typ(), nullptr, "tmp_bitmap");
     builder.CreateStore(i64val(0), i);
-    builder.CreateStore(i8val(0), j);
+
+    // as filter function takes uint8_t array as input, we need to cast it to field type
     auto *source = builder.CreatePointerCast(arg_source, ptr_from_arrow(*m_field->type()));
     builder.CreateBr(cond_1);
 
     builder.SetInsertPoint(cond_1);
+    // for(int i=0; i<arg_len / 8; i++)
     auto condition =
         builder.CreateICmpSLT(builder.CreateLoad(i), builder.CreateUDiv(arg_len, i64val(8)));
     builder.CreateCondBr(condition, for_1, end_for1);
 
     builder.SetInsertPoint(for_1);
     auto li = builder.CreateLoad(i);
+    // src = source + i * 8
     auto src = builder.CreateInBoundsGEP(source, builder.CreateMul(li, i64val(8)), "src");
-    builder.CreateStore(i8val(0), j);
     builder.CreateStore(i8val(0), tmp_bitmap);
+
+    builder.CreateStore(i8val(0), j);
     builder.CreateBr(cond_2);
 
+    // for(int j=0; j<8; j++)
     builder.SetInsertPoint(cond_2);
     builder.CreateCondBr(builder.CreateICmpSLT(builder.CreateLoad(j), i8val(8)), for_2, end_for2);
 
     builder.SetInsertPoint(for_2);
+
+    // bit = predicate(src[j]);
     auto bit = builder.CreateIntCast(
         builder.CreateCall(
             module.getFunction(m_field->name() + "_predicate"),
             {builder.CreateLoad(builder.CreateInBoundsGEP(src, builder.CreateLoad(j)))}),
         i8_typ(), false);
 
+    // bit_with_shift = bit << (7 - j)
     auto bit_with_shift =
         builder.CreateShl(bit, builder.CreateSub(i8val(7), builder.CreateLoad(j)));
 
+    // tmp_bitmap |= bit_with_shift
     auto updated_tmp_bitmap = builder.CreateOr(builder.CreateLoad(tmp_bitmap), bit_with_shift);
     builder.CreateStore(updated_tmp_bitmap, tmp_bitmap);
 
+    // j++
     builder.CreateStore(builder.CreateAdd(builder.CreateLoad(j), i8val(1)), j);
     builder.CreateBr(cond_2);
 
+    // end inner loop
     builder.SetInsertPoint(end_for2);
 
+    // dst[i] &= tmp_bitmap
     auto dest_val = builder.CreateLoad(builder.CreateInBoundsGEP(arg_dest, builder.CreateLoad(i)));
     auto updated_bitmap = builder.CreateAnd(dest_val, builder.CreateLoad(tmp_bitmap));
-
     builder.CreateStore(updated_bitmap, builder.CreateInBoundsGEP(arg_dest, builder.CreateLoad(i)));
+
+    // i++
     builder.CreateStore(builder.CreateAdd(builder.CreateLoad(i), i64val(1)), i);
     builder.CreateBr(cond_1);
 
+    // end outer loop
     builder.SetInsertPoint(end_for1);
     builder.CreateRetVoid();
   }
