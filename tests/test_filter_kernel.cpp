04c4fdc4566e481ee7ccb40d67734abbc73f2066
Artem Alekseev <fexolm@gmail.com>
2020-06-15 16:41:29 +0300

    feat: Add filter remaining function

--- tests/test_filter_kernel.cpp
+++ tests/test_filter_kernel.cpp
@@ -67,3 +67,30 @@ TYPED_TEST(FilterKernelOffsetsTest, testWithOffset10) {
   this->m_filter->execute(this->m_array, this->m_bitmap->mutable_data(), 10);
   arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b11111111, 0b11111111}));
 }
+
+TYPED_TEST(FilterKernelOffsetsTest, testRemainingBeginOffset2) {
+  this->m_filter->execute_remaining(this->m_array, this->m_bitmap->mutable_data(), 0, 2);
+  arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b11010110, 0b11111111}));
+}
+
+TYPED_TEST(FilterKernelOffsetsTest, testRemainingBeginOffset4) {
+  this->m_filter->execute_remaining(this->m_array, this->m_bitmap->mutable_data(), 0, 4);
+  arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b11110101, 0b11111111}));
+}
+
+TYPED_TEST(FilterKernelOffsetsTest, testRemainingEndOffset2) {
+  this->m_filter->execute_remaining(this->m_array, this->m_bitmap->mutable_data(), 0, 4);
+  arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b11110101, 0b11111111}));
+}
+
+TYPED_TEST(FilterKernelOffsetsTest, testRemainingEnd2) {
+  this->m_filter->execute_remaining(this->m_array, this->m_bitmap->mutable_data(),
+                                    this->m_array->length() - 2, 0);
+  arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b10111111, 0b11111111}));
+}
+
+TYPED_TEST(FilterKernelOffsetsTest, testRemainingEnd4) {
+  this->m_filter->execute_remaining(this->m_array, this->m_bitmap->mutable_data(),
+                                    this->m_array->length() - 4, 0);
+  arrow::AssertBufferEqual(*this->m_bitmap, std::vector<uint8_t>({0b00101111, 0b11111111}));
+}
